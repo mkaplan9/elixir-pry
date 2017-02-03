4da1713873fe36ad09fdde2383c4e4f34bcb2f86
Max Kaplan <max.m.kaplan@gmail.com>
2017-02-03 13:33:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# elixir-pry package
+
+Adds IEx.pry where your cursor is and require IEx add the top of the file
---
+++ spec/elixir-pry-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ElixirPryView from '../lib/elixir-pry-view';
+
+describe('ElixirPryView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
